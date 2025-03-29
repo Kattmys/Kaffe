db8bacdbe53e313f6d9c635f5e138b386f4ad2f5
jackiboi307 <jack.josefsson@gmail.com>
2025-03-29 10:18:36 +0100

    fixade id:s

--- main.py
+++ main.py
@@ -15,39 +15,50 @@ DATA  = "../Data/Kaffe/data.json"
  
 @app.route('/forum/submit', methods=['POST'])
 def process_form():
+    rubrik = flask.request.form.get('rubrik')
+    text = flask.request.form.get('text')
+    namn = flask.request.form.get('namn')
+    unix = time.time()
+
     with open(DATA) as file:
         data = json.load(file)
     id_ = data["next_id"]
     data["next_id"] += 1
     with open(DATA, "w") as file:
         json.dump(data, file)
-
-    rubrik = flask.request.form.get('rubrik')
-    text = flask.request.form.get('text')
-    namn = flask.request.form.get('namn')
-    unix = time.time()
     
-    with open(POSTS, "a") as file:
-        file.write(json.dumps({
-            "rubrik": rubrik,
-            "text": text,
-            "namn": namn,
-            "unix": unix,
-            "id":   id_,
-        }) + "\n")
+    with open(POSTS) as file:
+        posts = json.load(file)
+
+    posts[str(id_)] = {
+        "rubrik": rubrik,
+        "text": text,
+        "namn": namn,
+        "unix": unix,
+    }
+
+    with open(POSTS, "w") as file:
+        json.dump(posts, file)
     
     return flask.redirect("/forum")
  
 @app.route("/forum")
 def page():
     with open(POSTS) as file:
-        data = [json.loads(line) for line in file.read().splitlines()]
-        for inlägg in data:
-            if "unix" in inlägg:
-                inlägg["klartid"] = datetime.fromtimestamp(inlägg["unix"]).isoformat(" ", "seconds")
-        return flask.render_template(f"forum.html",
-            data=data
-        )
+        data = json.load(file)
+
+    posts = []
+
+    for id_ in data:
+        inlägg = data[id_]
+        inlägg["id"] = id_
+        if "unix" in inlägg:
+            inlägg["klartid"] = datetime.fromtimestamp(inlägg["unix"]).isoformat(" ", "seconds")
+        posts.append(inlägg)
+
+    return flask.render_template(f"forum.html",
+        data=posts
+    )
         
 @app.route("/kaffepress")
 def kaffepress():
